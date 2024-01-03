18a0046643c776ad5cbd01f4637752e799711bc8
Blake Patches <blakepatches@gmail.com>
2024-01-03 11:05:56 -0500

    Add dice shuffle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 var timerStarted = false;
 var currentTime;
 var timerDuration = currentTime = 5;
+var diceArray = [
+	"AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS",
+	"AOOTTW", "CIMOTU", "DEILRX", "DELRVY",
+	"DISTTY", "EEGHNW", "EEINSU", "EHRTVW",
+	"EIOSST", "ELRTTY", "HIMNQU", "HLNNRZ"
+];
 
 function timerClicked(el) {
 	if (!timerStarted) {
+		populateDice();
 		timerStarted = true;
 		el.style.display = "none";
 		document.getElementById("timer-progress").style.width = "100%";
@@ -13,7 +20,7 @@ function timerClicked(el) {
 
 function updateTimer() {
 	var timerPercentage = (currentTime / timerDuration) * 100;
-	document.getElementById("timer-progress").style.width = `${timerPercentage}%`;
+	document.getElementById("timer-progress").style.width = timerPercentage + "%";
 	if (currentTime > 0) {
         currentTime--;
         setTimeout(updateTimer, 1000);
@@ -25,3 +32,26 @@ function updateTimer() {
     	timerStarted = false;
     }
 }
+
+function populateDice() {
+	var shuffledDice = shuffleArray(diceArray);
+	var diceElements = document.getElementsByClassName("dice");
+	var selectedFace;
+	for (var i=0; i<diceElements.length; i++){
+		selectedFace = shuffledDice[i].charAt(Math.floor(Math.random() * 6));
+		diceElements[i].textContent = selectedFace == "Q" ? "Qu" : selectedFace;
+	}
+}
+
+function shuffleArray(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+document.addEventListener("DOMContentLoaded", populateDice);
